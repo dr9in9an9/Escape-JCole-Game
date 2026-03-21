a3ccfd27364e62c3534cfca89418fc2fc72e22a3
lukejohnson04 <notapollogising@gmail.com>
2026-03-21 18:47:16 -0400

    Got pathfinding working

--- JCole.gd
+++ JCole.gd
@@ -15,10 +15,10 @@ func _physics_process(delta):
 	right = right.normalized()
 
 	if path.size() > 0:
-		var top = path.get(0)
+		var top = path.get(0) + Vector2(0.5, 0.5)
 		while path.size() > 0 and top.distance_to(Vector2(position.x, position.z)) < 0.1:
 			path.remove_at(0)
-			top = path.get(0)
+			top = path.get(0) + Vector2(0.5, 0.5)
 		move_delta = (Vector3(top.x, 0, top.y) - position)
 		move_delta.y = 0
 		move_delta = move_delta.normalized() * MOVE_SPEED
@@ -27,7 +27,9 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _on_timer_timeout():
+	path.clear()
 	path = Level.astar.get_point_path(Vector2i(position.x, position.z), Vector2i(Level.player.position.x, Level.player.position.z))
-	if path.get(0).distance_to(Vector2i(position.x, position.z)) < 0.25:
-		path.remove_at(0)
+	#if (path.get(0) + Vector2(0.5, 0.5)).distance_to(Vector2i(position.x, position.z)) < 0.25:
+	path.remove_at(0)
+	path.remove_at(1)
 	print(path.size())
--- Level.gd
+++ Level.gd
@@ -9,23 +9,38 @@ var grid_height = 100
 
 func _ready():
 	astar = AStarGrid2D.new()
-	astar.region = Rect2i(-grid_width, -grid_height, grid_width, grid_height)
+	astar.region = Rect2i(-grid_width, -grid_height, grid_width*2, grid_height*2)
 	astar.cell_size = Vector2(1, 1)
 	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
 	
 	astar.update()
 	
 	for wall in $Walls.get_children():
+		if wall.position.y != 1:
+			continue
 		var wall_width = wall.scale.x;
 		var wall_height = wall.scale.z;
 		
-		var left = wall.position.x - wall_width * 0.5
-		var right = wall.position.x + wall_width * 0.5
-		var top = wall.position.z - wall_height * 0.5
-		var bottom = wall.position.z + wall_height * 0.5
-		var region = Rect2i(floor(left), ceil(right), floor(top), ceil(bottom))
-
+		var left = floor(wall.position.z - 0.5 - wall_width * 0.5)
+		var right = ceil(wall.position.z - 0.5 + wall_width * 0.5)
+		var top = floor(wall.position.x - 0.5 - wall_height * 0.5)
+		var bottom = ceil(wall.position.x - 0.5 + wall_height * 0.5)
+		var region = Rect2i(left, right - left + 1, top, bottom - top + 1)
+		#var region = get_wall_rect(wall)
+		#print(region)
+		#
 		astar.fill_solid_region(region, true)
 	
 	#$Player.player_path = Level.astar.get_point_path(Vector2i($Player.position.x, $Player.position.z), Vector2i(5, 2))
 	#print($Player.player_path.size())
+
+func get_wall_rect(wall: Node3D) -> Rect2i:
+	var size = Vector2i(wall.scale.x, wall.scale.z)
+	var center := Vector2(wall.global_position.x - 0.5, wall.global_position.z - 0.5)
+
+	var top_left := Vector2i(
+		center.x - int(floor(size.x / 2.0)),
+		center.y - int(floor(size.y / 2.0))
+	)
+
+	return Rect2i(top_left, size)
